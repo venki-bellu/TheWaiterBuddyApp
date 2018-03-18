02fcc804d8a54002b4083c1bdb6fff348efe31ca
venkibellu <venkateshrbelavadi@gmail.com>
2018-03-18 09:46:08 +0530

    Implemented receiving of data in CheckoutClass

--- app/src/main/java/uvce/com/thewaiterbuddy/CheckoutActivity.java
+++ app/src/main/java/uvce/com/thewaiterbuddy/CheckoutActivity.java
@@ -9,6 +9,9 @@ import java.util.ArrayList;
 public class CheckoutActivity extends AppCompatActivity {
     private final static String TAG = "APPATHON";
     private ArrayList<CartItem> checkoutItems;
+    private ArrayList<String> foodItemNames=new ArrayList<>();
+    private ArrayList<Double> foodItemsCost=new ArrayList<>();
+    private ArrayList<Integer> foodItemsQuantity=new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +20,24 @@ public class CheckoutActivity extends AppCompatActivity {
 
         checkoutItems = getIntent().getParcelableArrayListExtra(CartItem.TYPE);
 
-        Log.i(TAG, checkoutItems.size() + " items in cart.");
+       /* Log.i(TAG, checkoutItems.size() + " items in cart.");
         for (CartItem item : checkoutItems) {
             Log.i(TAG, item.getName() + " x " + item.getQuantity());
+
+        }*/
+
+        //Getting the data of all the checkout items
+        for(int i=0;i<checkoutItems.size();i++)
+        {
+            foodItemNames.add(checkoutItems.get(i).getName());
+            foodItemsCost.add(checkoutItems.get(i).getCost());
+            foodItemsQuantity.add(checkoutItems.get(i).getQuantity());
+            Log.i(TAG, foodItemNames.get(i));
+            Log.i(TAG,""+ foodItemsCost.get(i));  //Added to a string to parse into String from double
+            Log.i(TAG,""+foodItemsQuantity.get(i));
+
         }
+
+
     }
 }
