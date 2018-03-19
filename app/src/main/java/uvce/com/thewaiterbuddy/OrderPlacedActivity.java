88074ea15ab0990ee5c2d56036bb833997297a5f
venkibellu <venkateshrbelavadi@gmail.com>
2018-03-19 23:50:33 +0530

    Added Place New Order Option

--- app/src/main/java/uvce/com/thewaiterbuddy/OrderPlacedActivity.java
+++ app/src/main/java/uvce/com/thewaiterbuddy/OrderPlacedActivity.java
@@ -1,7 +1,10 @@
 package uvce.com.thewaiterbuddy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 public class OrderPlacedActivity extends AppCompatActivity {
@@ -17,5 +20,22 @@ public class OrderPlacedActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         Toast.makeText(this,"Order is placed and can't be cancelled now",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.order_placed_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.newOrder)
+        {
+            startActivity(new Intent(OrderPlacedActivity.this,LogInActivity.class));
+            finish();
+            return true;
+        }
+        return false;
     }
 }
